78e47e0a3a165f6df530b02de71a53887aa76d87
Aleksey <aliaksei.balabushka@gmail.com>
2022-05-09 17:47:48 +0300

    feat: add new symbol in textArea (with use physical keyboard)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import KEYS from './scripts/keys'
 import './styles/style.scss'
 
+// Global variables
+let cursorPosition = null
+
+/* --------------------------------------------------------------------------------- */
+
 const createBaseElement = () => {
   const main = document.createElement('main')
   document.body.appendChild(main)
@@ -61,8 +66,178 @@ const addPlugToRow = rowEl => {
   rowEl.appendChild(plugEl)
 }
 
-// Start
+/* --------------------------------------------------------------------------------- */
+
+const onKeyDown = event => {
+  event.preventDefault()
+
+  const { key } = event
+  const textAreaEl = document.getElementsByClassName('screen')[0]
+
+  textAreaEl.value = key.length < 2
+    ? onSymbolKeyDown(textAreaEl, key)
+    : onSpecialKeyDown(textAreaEl, key)
+}
+
+const onSymbolKeyDown = (textAreaEl, key) => {
+  let { value } = textAreaEl
+  let [cursorStart, cursorEnd] = cursorPosition
+  let newValue = ''
+
+  if (cursorStart === cursorEnd) {
+    [newValue, cursorStart, cursorEnd] = cursorEnd === value.length
+      ? addValueInTheEnd(value, key)
+      : addValueInside(value, key, cursorStart, cursorEnd)
+  } else {
+    [newValue, cursorStart, cursorEnd] = cursorEnd === value.length
+      ? replaceValueInTheEnd(value, key, cursorStart, cursorEnd)
+      : replaceValueInside(value, key, cursorStart, cursorEnd)
+  }
+
+  moveCursor(textAreaEl, cursorStart, cursorEnd)
+  return newValue
+}
+
+const onSpecialKeyDown = (textAreaEl, key) => {
+  let { value } = textAreaEl
+  let [cursorStart, cursorEnd] = cursorPosition
+  let newValue = ''
+
+  switch (key) {
+
+    case 'Tab':
+      if (cursorStart === cursorEnd) {
+        [newValue, cursorStart, cursorEnd] = cursorEnd === value.length
+          ? addValueInTheEnd(value, '\t')
+          : addValueInside(value, '\t', cursorStart, cursorEnd)
+      } else {
+        [newValue, cursorStart, cursorEnd] = cursorEnd === value.length
+          ? replaceValueInTheEnd(value, '\t', cursorStart, cursorEnd)
+          : replaceValueInside(value, '\t', cursorStart, cursorEnd)
+      }
+      break
+
+    case 'Enter':
+      if (cursorStart === cursorEnd) {
+        [newValue, cursorStart, cursorEnd] = cursorEnd === value.length
+          ? addValueInTheEnd(value, '\n')
+          : addValueInside(value, '\n', cursorStart, cursorEnd)
+      } else {
+        [newValue, cursorStart, cursorEnd] = cursorEnd === value.length
+          ? replaceValueInTheEnd(value, '\n', cursorStart, cursorEnd)
+          : replaceValueInside(value, '\n', cursorStart, cursorEnd)
+      }
+      break
+
+    case 'Backspace':
+      if (cursorStart === cursorEnd) {
+        cursorEnd === value.length
+          ? (newValue = value.slice(0, value.length - 1),
+            cursorStart = cursorEnd = newValue.length)
+          : (newValue = value.slice(0, cursorStart - 1) + value.slice(cursorEnd),
+            cursorStart = cursorEnd = cursorEnd - 1)
+      } else {
+        cursorEnd === value.length
+          ? (newValue = value.slice(0, cursorStart),
+            cursorStart = cursorEnd = newValue.length)
+          : (newValue = value.slice(0, cursorStart) + value.slice(cursorEnd),
+            cursorEnd = cursorStart)
+      }
+      break
+
+    case 'Delete':
+      if (cursorStart === cursorEnd) {
+        cursorEnd === value.length
+          ? (newValue = value,
+            cursorStart = cursorEnd = value.length)
+          : (newValue = value.slice(0, cursorStart) + value.slice(cursorEnd + 1),
+            cursorStart = cursorEnd)
+      } else {
+        cursorEnd === value.length
+          ? (newValue = value.slice(0, cursorStart),
+            cursorStart = cursorEnd = newValue.length)
+          : (newValue = value.slice(0, cursorStart) + value.slice(cursorEnd),
+            cursorEnd = cursorStart)
+      }
+      break
+
+    // Arrows
+    case 'ArrowUp':
+      return value += '▲'
+    case 'ArrowRight':
+      return value += '►'
+    case 'ArrowDown':
+      return value += '▼'
+    case 'ArrowLeft':
+      return value += '◄'
+
+    // Other keys
+    default: return value
+
+  }
+
+  moveCursor(textAreaEl, cursorStart, cursorEnd)
+  return newValue
+}
+
+
+
+/* Change cursor position & textarea value */
+const addValueInTheEnd = (value, key) => {
+  let v = value + key
+  return [v, v.length, v.length]
+}
+
+const addValueInside = (value, key, cursorStart, cursorEnd) => {
+  let v = value.slice(0, cursorStart) + key + value.slice(cursorEnd)
+  cursorStart = cursorEnd = cursorEnd + 1
+  return [v, cursorStart, cursorEnd]
+}
+
+const replaceValueInTheEnd = (value, key, cursorStart, cursorEnd) => {
+  let v = value.slice(0, cursorStart) + value.slice(cursorEnd) + key
+  return [v, value.length, value.length]
+}
+
+const replaceValueInside = (value, key, cursorStart, cursorEnd) => {
+  let v = value.slice(0, cursorStart) + key + value.slice(cursorEnd, value.length)
+  cursorEnd = cursorStart = cursorStart + 1
+  return [v, cursorStart, cursorEnd]
+}
+
+const moveCursor = (el, start, end) => setTimeout(() => el.setSelectionRange(start, end))
+
+
+/* ----- START --------------------------------------------------------------------- */
+
 window.onload = () => {
+
+  // Add textarea and keyboard
   createBaseElement()
   addRowToKeyboard()
+
+
+  /* Find cursor position */
+  const textAreaEl = document.getElementsByClassName('screen')[0]
+  textAreaEl.focus()
+
+  document.addEventListener('keydown', event => {
+    textAreaEl.focus()
+    cursorPosition = [event.target.selectionStart, event.target.selectionEnd]
+    onKeyDown(event)
+    console.log('keydown: ', cursorPosition)
+  })
+
+  document.addEventListener('click', () => {
+    const { value } = textAreaEl
+    cursorPosition = [value.length, value.length]
+    console.log('клик вне: ', cursorPosition)
+  })
+
+  textAreaEl.addEventListener('click', event => {
+    event.stopPropagation()
+    cursorPosition = [event.target.selectionStart, event.target.selectionEnd]
+    console.log('click: ', cursorPosition)
+  })
+  /* Find cursor position — End */
 }
